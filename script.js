a9c97c08f273a9136aece3af984dcc0eed4b4bb6
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-03-05 21:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+
+function getNumbers() {
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4]);
+		},3000);
+	}) 
+}
+getNumbers()
+	.then((numbers)=>{
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			let evenNum=numbers.filter(num=>num%2 === 0);
+			document.getElementById("output").textContent=JSON.stringify(evenNum);
+			resolve(evenNum);
+		},1000)
+	})
+	}).then((evenNum)=>{
+		return new Promise((resolve)=>{
+		setTimeout(()=>{
+			let multiply=evenNum.map(num=>num*2);
+			document.getElementById("output").textContent=JSON.stringify(multiply);
+			resolve(multiply);
+		},2000);
+		})
+		
+	})
